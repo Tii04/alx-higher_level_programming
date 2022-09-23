16a0bfbb430a75cd5296c6cb1221af6715ebe20c
Tii04 <Tiisetso.qsebata@gmail.com>
2022-09-23 10:30:43 -0700

    Numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,8 +4,7 @@ for i in range(0, 10):
     for x in range(0, 10):
         if i >= x:
             continue
-
-        elif i == 8 and i == 9:
-             print("{}}{}".format(i, x), end="")
+        elif i == 8 and x == 9:
+             print("{}{}".format(i, x), end="")
         else:
              print("{}{}, ".format(i, x), end="")
