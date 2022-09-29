63e91beeba43ee696f151fb44915127bfceeda2d
Tii04 <Tiisetso.qsebata@gmail.com>
2022-09-29 02:13:58 -0700

    Replacement

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx > len(my_list) - 1:
+        return my_list
+    my_list.pop(idx)
+    my_list.insert(idx, element)
+    return my_list
+
