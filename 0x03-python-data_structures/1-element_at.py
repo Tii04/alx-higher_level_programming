f946410423d81171984286e53f175bf06dc32fb7
Tii04 <Tiisetso.qsebata@gmail.com>
2022-09-29 01:51:39 -0700

    Which element?

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 :
+    if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx > len(my_list) -1:
         return None
     ele = my_list.pop(idx)
     return ele
