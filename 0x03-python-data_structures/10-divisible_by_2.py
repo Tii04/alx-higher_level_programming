b36d20278bb9a60d60dedaa9db1ae7266d2a5858
Tii04 <Tii04@github.com>
2022-09-30 19:57:29 +0200

    Divisable?

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 
 def divisible_by_2(my_list=[]):
     boolean = my_list[:]
-    for count, i in enumerate (my_list):
+    for count, i in enumerate(my_list):
         if i % 2 == 0:
             boolean[count] = True
         else:
