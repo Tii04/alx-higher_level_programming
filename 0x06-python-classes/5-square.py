c5623784b8da67cc7bf86130e829d9e9edc7c920
Tii04 <Tii04@github.com>
2022-10-14 12:31:19 +0200

    #squared

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+'''Defines a square'''
+
+
+class Square():
+
+    """Size is the size of a square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method"""
+
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter method. value should be of type int."""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise valueError("size must >= 0")
+
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(0, self.__size):
+            for s in range(0, self.__size):
+                print("#", end="")
+            print()
