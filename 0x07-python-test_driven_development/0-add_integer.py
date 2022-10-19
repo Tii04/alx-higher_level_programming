7f34ea562e920569e22c318396945a72ba1a018e
Tii04 <Tii04@github.com>
2022-10-19 16:58:50 +0200

    Hot fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-""" Defines a function that will add numbers"""
+""" Defines a function that will add numbers
+    Function takes floats or integers as arguments
+    and returns a number of type int.
+    If the function is not given int or float, TyperError is raised.
+"""
 
 
 def add_integer(a, b=98):
-    """
-    Function takes both floats and integers but the return
-    is typecasted to int.
-
-    TypeError raised if a or b is not an int or float.
-    """
+    """ Arguments: floats and integers.
+    Return: typecasted to int.
+    Otherwise, TyperError raised"""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
