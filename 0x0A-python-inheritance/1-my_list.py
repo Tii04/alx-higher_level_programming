72e9b0c76dfeea903e1658549a8b6c75fe8e5951
Tii04 <Tii04@github.com>
2022-10-24 20:24:46 +0200

    Ascending order

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Class MyList is subclass of class list
+"""
+
+
+class MyList(list):
+    pass
+
+    def print_sorted(self):
+        """
+        Function prints a sorted list
+        in ascending order, assuming all elements
+        of list are of type int.
+        """
+
+        print(sorted(self))
