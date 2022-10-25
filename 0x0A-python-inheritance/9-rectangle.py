dafb506eddd2477a52d8d6f4dea3812b9205391c
Tii04 <Tii04@github.com>
2022-10-25 06:23:16 +0200

    REctangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-"""
-Additional implementation of base class
-"""
 
+"""
+Implementation of inheritance
+"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ definition of a Rectangle """
+    '''
+    Implements a rectangle
+    '''
 
     def __init__(self, width, height):
-        """ constructor and width, height"""
+        """ Initialises parameters"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
         self.__width = width
         self.__height = height
-        BaseGeometry.integer_validator(self, "width", self.__width)
-        BaseGeometry.integer_validator(self, "height", self.__height)
 
-        def area(self):
-            return self.__width * self.__height
+    def area(self):
+        return self.__width * self.__height
 
     def __str__(self):
-        """ print """
-        return ("[Rectangle] " + str(self.__width) + "/" + str(self.__height))
+        """ Specifies how printing instance objects should be"""
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
