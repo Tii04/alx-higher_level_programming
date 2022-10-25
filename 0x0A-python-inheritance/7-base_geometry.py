8c686a72cda91817f73a81f69f0dd5fd30068728
Tii04 <Tii04@github.com>
2022-10-25 05:06:46 +0200

    Area

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Class that defines a geometry module
+"""
+
+
+class BaseGeometry():
+    """
+    Class that represents a geometry module
+    """
+
+    # def __init__(self, name, value):
+    #   """
+    #   Initialising parameters
+    #    """
+
+    #   self.name = name
+    #   self.value = value */
+
+    def area(self):
+        """
+        Function that raises exception error
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        function that validates the value.
+        Value has to be int, name must be a string
+        greater than 0. Otherwise exception raised.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
