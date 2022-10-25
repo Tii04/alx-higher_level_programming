332ee1733ae3601908cc62ac730e81778afb31bb
Tii04 <Tii04@github.com>
2022-10-25 04:50:01 +0200

    Not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Class that defines a geometry module
+"""
+
+
+class BaseGeometry():
+    """
+    Class that represents a geometry module
+    """
+
+    def area(self):
+        """
+        Function that raises exception error
+        """
+
+        raise Exception("area() is not implemented")
