e0d74e5f8c98489da68fa4d3ae6e7528067a7f34
Tii04 <Tii04@github.com>
2022-10-26 15:44:54 +0200

    JSON

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Function that returns JSON representation of object.
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Function returns JSON representation of object.
+    my_obj is of type str.
+    """
+
+    return json.dumps(my_obj)
