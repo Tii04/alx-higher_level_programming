34132a8e459bfa542241b11919abce6d896635a7
Tii04 <Tii04@github.com>
2022-11-01 17:36:39 +0200

    Dummy

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,6 +44,21 @@ class Base():
             return json.loads(json_string)
 
     @classmethod
+    def create(cls, **dictionary):
+        '''
+            Returns an instance with all the attributes already set
+        '''
+        from models.rectangle import Rectangle
+        from models.square import Square
+
+        if cls.__name__ == "Rectangle":
+            r2 = Rectangle(3, 8)
+        elif cls.__name__ == "Square":
+            r2 = Square(5)
+        r2.update(**dictionary)
+        return (r2)
+
+    @classmethod
     def save_to_file(cls, list_objs):
         """ JSON representation to a file"""
 
