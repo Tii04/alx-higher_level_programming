386aa3db80afd36703146f76ea7c25f2ae3537a9
Tii04 <Tii04@github.com>
2022-11-01 14:03:56 +0200

    JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@
 This is the base class.
 """
 
+import json
+
 
 class Base():
     """
@@ -20,3 +22,12 @@ class Base():
         else:
             Base. __nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """JSON representation of list_dictionaries"""
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
