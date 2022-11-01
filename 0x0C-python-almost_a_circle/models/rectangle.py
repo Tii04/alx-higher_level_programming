fa65817c393b14684b9d2cc8e5a3bbf11db1baf7
Tii04 <Tii04@github.com>
2022-11-01 04:33:25 +0200

    Assignment

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,6 +95,18 @@ class Rectangle(Base):
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
                 self.y, self.width, self.height))
 
+    def update(self, *args):
+        """ Assigns an argument to each attribute"""
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+
+        except IndexError:
+            pass
+
     @staticmethod
     def valid_attr(attr, value):
         if type(value) != int:
