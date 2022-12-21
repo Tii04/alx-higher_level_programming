9e75c8f7f55c2bf7b7ab51dcbb238b1c208fb092
Tii04 <Tii04@github.com>
2022-12-21 18:03:38 +0200

    Languages

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 
 if (!isNaN(process.argv[2])) {
-  for (let i = parseInt(process.argv[2]); i > 0; i--) {
-    console.log('C is fun');
-  }
+    for (let i = parseInt(process.argv[2]); i > 0; i--) {
+      console.log('C is fun');
+    }
+} else {
+    console.log('Missing number of occurreneces');
 }
+
