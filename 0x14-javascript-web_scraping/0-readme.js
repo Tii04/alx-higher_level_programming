6775a42eb6c7311cc1e2272ae2a98748ac17b342
Tii04 <Tii04@github.com>
2023-02-11 21:54:42 +0200

    README SemiStandard compliance

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,5 @@ const file = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(file, 'utf8', (err, data) => {
-	if (err) 
-		console.log(err);
-	else
-		console.log(data);
-	});
+  if (err) { console.log(err); } else { console.log(data); }
+});
