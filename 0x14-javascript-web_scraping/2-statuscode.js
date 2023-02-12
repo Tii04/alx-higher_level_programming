abdc2b0ac2b04acc7f18406b92b3069861f97542
Tii04 <Tii04@github.com>
2023-02-12 22:36:19 +0200

    Semistandard fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,5 @@ const url = process.argv[2];
 const request = require('request');
 
 request(url, (err, response, body) => {
-  if (err)
-    console.log('error:', err);
-  else
-    console.log('code:', response && response.statusCode);
-  });
+  if (err) { console.log('error:', err); } else { console.log('code:', response && response.statusCode); }
+});
