d9c90795d7efbbd0c7ce10fbac0c661d4aae9bb5
Tii04 <Tii04@github.com>
2023-02-12 22:30:48 +0200

    Status Code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+
+request(url, (err, response, body) => {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log('code:', response && response.statusCode);
+  });
